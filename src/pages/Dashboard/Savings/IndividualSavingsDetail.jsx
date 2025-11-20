621dcbd66b406ea768aa9263dd4f98678977b209
Khemmie-Ray <atokemmy@gmail.com>
2025-11-20 10:07:21 +0100

    modifications to fetch

--- src/pages/Dashboard/Savings/IndividualSavingsDetail.jsx
+++ src/pages/Dashboard/Savings/IndividualSavingsDetail.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import { DashNav } from "../../../components/shared/Reuse";
 import { MobileDashNav } from "../../../components/shared/Reuse";
 import { IoIosArrowBack } from "react-icons/io";
@@ -11,24 +11,22 @@ import Loader from "../../../components/loaders/Loader";
 import Saveindividual from "../../../components/dashboard/Saveindividual";
 import Withdraw from "../../../components/dashboard/Withdraw";
 import NextTime from "../../../components/dashboard/NextTime";
+import { getReadableDate } from "../../../components/shared/Reuse";
+import EmergencyWithdraw from "../../../components/dashboard/EmergencyWithdraw";
 
 const IndividualSavingsDetail = () => {
   const { id } = useParams();
   const location = useLocation();
   const { thriftAddress } = location.state || {};
   const { singleThriftUser } = useFetchIndividual();
-  // const { address } = useAppKitAccount()
+
   if (!singleThriftUser || singleThriftUser.length === 0) {
     return <Loader />;
   }
-  console.log("Help:", thriftAddress)
-
 
   const selectedGoal = singleThriftUser?.find(
     (item) => item.goalId === Number(id)
   );
-
-  console.log(selectedGoal)
 
   const getTokenDecimals = (currencyAddress) => {
     const token = tokenList[currencyAddress];
@@ -45,18 +43,12 @@ const IndividualSavingsDetail = () => {
     );
   };
 
-  const getReadableDate = (timestamp) => {
-    if (!timestamp) return "-";
-    const date = new Date(Number(timestamp) * 1000);
-    return date.toLocaleDateString(undefined, {
-      year: "numeric",
-      month: "short",
-      day: "numeric",
-    });
-  };
-
   const goal = getReadableAmount(selectedGoal.goal, selectedGoal.currency);
   const saved = getReadableAmount(selectedGoal.saved, selectedGoal.currency);
+  const amountToSave = getReadableAmount(
+    selectedGoal.amountPerPeriod,
+    selectedGoal.currency
+  );
   const percent =
     (parseFloat(selectedGoal.saved) / parseFloat(selectedGoal.goal)) * 100;
   const frequencyOptions = ["daily", "weekly", "bi-weekly", "monthly"];
@@ -66,6 +58,10 @@ const IndividualSavingsDetail = () => {
   const rawGoal = parseFloat(formatUnits(selectedGoal.goal, decimals));
   const rawSaved = parseFloat(formatUnits(selectedGoal.saved, decimals));
   const rawLeft = rawGoal - rawSaved;
+  const currencyAddress = selectedGoal.currency;
+  const tokenInfo = tokenList[currencyAddress];
+
+  const currency = tokenInfo ? `${tokenInfo.symbol}` : "Unknown Token";
 
   const displayLeft = rawLeft.toLocaleString(undefined, {
     maximumFractionDigits: 2,
@@ -89,13 +85,20 @@ const IndividualSavingsDetail = () => {
         </div>
         <div className="flex items-center">
           <div className="1/5">
-          <Saveindividual thriftAddress={thriftAddress} />
+            <Saveindividual
+              thriftAddress={thriftAddress}
+              amount={selectedGoal.amountPerPeriod}
+            />
           </div>
           <div className="1/5">
-          <Withdraw thriftAddress={thriftAddress} />
+            <Withdraw thriftAddress={thriftAddress} />
+          </div>
+          <div className="1/5">
+            <EmergencyWithdraw thriftAddress={thriftAddress} />
           </div>
         </div>
       </section>
+
       <section className="lg:px-8 md:px-8 px-4">
         <div className="w-[100%] flex justify-between items-center flex-col lg:flex-row md:flex-row mt-6 flex-wrap">
           <div className="flex items-center lg:w-[32%] md:w-[32%] w-[100%] mb-3 bg-white rounded-2xl p-3">
@@ -105,7 +108,10 @@ const IndividualSavingsDetail = () => {
             <div className="w-[75%]">
               <h3 className="text-[14px] font-[600]">Overview</h3>
               <p className="text-[14px] text-grey">
-                ${saved} / <span>${goal}</span>
+                {saved} /{" "}
+                <span>
+                  {goal} {currency}
+                </span>
               </p>
               <input
                 type="range"
@@ -116,7 +122,22 @@ const IndividualSavingsDetail = () => {
                 className="w-full h-2 custom-range"
               />
               <p className="text-grey text-[12px]">
-                {percent}% goal reached <span>Individual savings</span>
+                {Math.round(percent)}% goal reached <span>Individual savings</span>
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center lg:w-[32%] md:w-[32%] w-[100%] mb-3 bg-white rounded-2xl p-3">
+            <div className="bg-[#EAE3F8] flex justify-center items-center p-1 text-primary rounded-full w-[40px] h-[40px] text-2xl mr-2">
+              <BiLayout />
+            </div>
+            <div className="w-[75%]">
+              <h3 className="text-[14px] font-[600]">Amount to Save</h3>
+              <p className="text-[14px] text-grey">
+                {amountToSave} {currency}
+              </p>
+
+              <p className="text-grey text-[12px]">
+                You have contributed {saved} {currency}
               </p>
             </div>
           </div>
@@ -128,10 +149,12 @@ const IndividualSavingsDetail = () => {
               <h3 className="text-[14px] font-[600]">
                 Total amount contributed
               </h3>
-              <p className="text-[14px] text-grey">${saved}</p>
+              <p className="text-[14px] text-grey">
+                {saved} {currency}
+              </p>
 
               <p className="text-grey text-[12px]">
-                You have contributed ${saved}
+                You have contributed {saved} {currency}
               </p>
             </div>
           </div>
@@ -141,10 +164,12 @@ const IndividualSavingsDetail = () => {
             </div>
             <div className="w-[75%]">
               <h3 className="text-[14px] font-[600]">Total amount left</h3>
-              <p className="text-[14px] text-grey">${displayLeft}</p>
+              <p className="text-[14px] text-grey">
+                {displayLeft} {currency}
+              </p>
 
               <p className="text-grey text-[12px]">
-                You have ${displayLeft} left to meet your goals
+                You have {displayLeft} {currency} left to meet your goals
               </p>
             </div>
           </div>
