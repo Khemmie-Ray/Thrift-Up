91c184086067014b7a563c5c21f6ea2755123555
Khemmie-Ray <atokemmy@gmail.com>
2025-11-26 23:32:27 +0100

    fixes

--- src/components/dashboard/Withdraw.jsx
+++ src/components/dashboard/Withdraw.jsx
@@ -1,12 +1,11 @@
 import React, { useCallback, useState } from "react";
-import { Button } from "@headlessui/react";
 import { useAppKitAccount, useAppKitNetwork } from "@reown/appkit/react";
-import { coreTestnet2 } from "@reown/appkit/networks";
+import { baseSepolia } from "@reown/appkit/networks";
 import { toast } from "react-toastify";
 import { ErrorDecoder } from "ethers-decode-error";
 import abi from "../../constants/singlethriftAbi.json";
 import useSignerOrProvider from "../../hooks/useSignerOrProvider";
-import { Contract, ethers } from "ethers";
+import { ethers } from "ethers";
 import { BeatLoader } from "react-spinners";
 
 const Withdraw = ({ thriftAddress }) => {
@@ -14,12 +13,11 @@ const Withdraw = ({ thriftAddress }) => {
   const { address } = useAppKitAccount();
   const errorDecoder = ErrorDecoder.create([abi]);
   const { signer } = useSignerOrProvider();
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
   const contract = new ethers.Contract(thriftAddress, abi, signer);
 
   const handleWithdraw = useCallback(async () => {
-
     if (!address) {
       toast.error("Please connect your wallet");
       return;
@@ -30,13 +28,13 @@ const Withdraw = ({ thriftAddress }) => {
       return;
     }
 
-    if (Number(chainId) !== Number(coreTestnet2.id)) {
-      toast.error("You're not connected to Core Testnet2");
+    if (Number(chainId) !== Number(baseSepolia.id)) {
+      toast.error("You're not connected to Hedera");
       return;
     }
 
     try {
-      setLoading(true)
+      setLoading(true);
 
       const tx = await contract.withdraw();
       console.log(tx);
@@ -53,19 +51,18 @@ const Withdraw = ({ thriftAddress }) => {
         position: "top-center",
       });
     } finally {
-        setLoading(false)
+      setLoading(false);
     }
   }, [contract, address, chainId]);
 
   return (
     <>
-          <Button
-            className="border border-primary py-3 px-6 flex justify-center items-center rounded-full text-[14px] font-[500] hover:scale-105 text-primary mb-3  ml-4"
-            onClick={handleWithdraw}
-          >
-            {!loading ? "Withdraw" :     <BeatLoader color="#6138FE" size={15} />
-        }
-          </Button>
+      <button
+        className="border border-primary py-3 px-6 flex justify-center items-center rounded-full text-[14px] font-[500] hover:scale-105 text-primary mb-3  ml-4"
+        onClick={handleWithdraw}
+      >
+        {!loading ? "Withdraw" : <BeatLoader color="#6138FE" size={15} />}
+      </button>
     </>
   );
 };
